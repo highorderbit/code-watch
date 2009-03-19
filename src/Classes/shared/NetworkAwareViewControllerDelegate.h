e5e7d1bdad27ae337e5ba3237eb24f13912e034f
Doug Kurth <doug@highorderbit.com>
2009-03-18 19:07:56 -0600

    Refactored tab selection

---
+++ src/Classes/shared/NetworkAwareViewControllerDelegate.h
@@ -0,0 +1,16 @@
+//
+//  NetworkAwareViewControllerDelegate.h
+//  code-watch
+//
+//  Created by Douglas Kurth on 3/18/09.
+//  Copyright 2009 High Order Bit, Inc.. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface NetworkAwareViewControllerDelegate : NSObject {
+
+}
+
+@end
