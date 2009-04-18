a7cf902b2dfec49a549bce750af957711634fb27
John A. Debay <john@highorderbit.com>
2009-04-17 19:16:32 -0600

    Log memory warnings, and show an alert box on the first one. The alert view should be removed before submitting to Apple.

--- src/Classes/app-controller/CodeWatchAppDelegate.m
+++ src/Classes/app-controller/CodeWatchAppDelegate.m
@@ -3,6 +3,7 @@
 //
 
 #import "CodeWatchAppDelegate.h"
+#import "UIAlertView+CreationHelpers.h"
 
 @implementation CodeWatchAppDelegate
 
@@ -32,5 +33,24 @@
     [appController persistState];
 }
 
-@end
+- (void)applicationDidReceiveMemoryWarning:(UIApplication *)application
+{
+    static BOOL warningDisplayed = NO;
 
+    NSLog(@"WARNING: application received memory warning.");
+
+    if (!warningDisplayed) {
+        NSString * title = @"Memory Warning Received";
+        NSString * message =
+            @"The application has received a request to release some memory. "
+            "This alert will not be shown again, but future occurrences will "
+            "be logged.";
+        UIAlertView * alert =
+            [UIAlertView simpleAlertViewWithTitle:title message:message];
+        [alert show];
+
+        warningDisplayed = YES;
+    }
+}
+
+@end
